df41322413951d3caa8afb176819dbd40fe44f5e
audreycampdoras <audrey.campdoras@edu.ece.fr>
2021-10-07 23:51:40 +0200

    Refresh

--- js/script.js
+++ js/script.js
@@ -4,60 +4,58 @@ function start() {
   // Création de l'objet apiWeather
   const apiWeather = new API_WEATHER();
   // Appel de la fonction fetchTodayForecast
-
   apiWeather
     .fetchTodayForecast()
     .then(function (response) {
       // Récupère la donnée d'une API
       const data = response.data;
-
       // On récupère l'information principal
       const main = data.weather[0].main;
       const description = data.weather[0].description;
       const temp = data.main.temp;
       const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
-
       // Modifier le DOM
       document.getElementById('today-forecast-main').innerHTML = main;
       document.getElementById('today-forecast-more-info').innerHTML = description;
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-
     })
     .catch(function (error) {
       // Affiche une erreur
       console.error(error);
     });
+
 }
 
 // Mes modifications
 
 function city() {
-  var city = document.getElementById("city-input").value;
-  const apiWeather = new API_WEATHER(city);
+  var cityInput = document.getElementById("city-input").value;
+  // Création de l'objet apiWeather
+  const apiWeather = new API_WEATHER(cityInput);
+  // Appel de la fonction fetchTodayForecast
   apiWeather
     .fetchTodayForecast()
     .then(function (response) {
       // Récupère la donnée d'une API
       const data = response.data;
-
       // On récupère l'information principal
       const main = data.weather[0].main;
       const description = data.weather[0].description;
       const temp = data.main.temp;
       const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
-
       // Modifier le DOM
       document.getElementById('today-forecast-main').innerHTML = main;
       document.getElementById('today-forecast-more-info').innerHTML = description;
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-
     })
+
     .catch(function (error) {
       // Affiche une erreur
       console.error(error);
     });
 
-
 }
+
+
